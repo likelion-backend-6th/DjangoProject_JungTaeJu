fe8e431d4250e30a94b1a0251b8a833203ca49f5
taeshs <taejustar@gmail.com>
2023-08-01 18:03:34 +0900

    move codes

---
+++ blog/views.py
@@ -0,0 +1,84 @@
+from django.core.paginator import Paginator, EmptyPage, PageNotAnInteger
+from django.shortcuts import render, get_object_or_404
+from django.views.decorators.http import require_POST
+from django.views.generic import ListView
+
+from blog.forms import EmailPostForm, CommentForm
+from blog.models import Post
+from django.core.mail import send_mail
+from taggit.models import Tag
+from django.db.models import Count
+
+# Create your views here.
+
+def post_share(request, post_id):
+    # id로 게시물 조회
+    post = get_object_or_404(Post, id=post_id, status=Post.Status.PUBLISHED)
+    sent = False
+    if request.method == 'POST':
+        # 폼이 제출되었을 때
+        form = EmailPostForm(request.POST)
+        if form.is_valid():
+            # 폼 필드가 유효성 검사를 통과
+            cd = form.cleaned_data
+            post_url = request.build_absolute_uri(post.get_absolute_url())
+            subject = f"{cd['name']}님이 {post.title}을(를) 추천합니다."
+            message = f"{post.title}을(를) 다음에서 읽어보세요.\n\n" \
+                      f"{cd['name']}의 의견: {cd['comments']}"
+            send_mail(subject, message, 'taejustar@gmail.com', [cd['to']])
+            sent = True
+    else:
+        form = EmailPostForm()
+    return render(request, 'blog/post/share.html', {'post': post, 'form': form, 'sent': sent})
+
+
+class PostListView(ListView):
+    queryset = Post.published.all()
+    context_object_name = 'posts'
+    paginate_by = 3
+    template_name = 'blog/post/list.html'
+
+
+def post_list(request, tag_slug=None):
+    post_list = Post.published.all()
+    tag=None
+    if tag_slug:
+        tag = get_object_or_404(Tag, slug=tag_slug)
+        post_list = post_list.filter(tags__in=[tag])
+    per_page = request.GET.get('per_page', 5)
+    paginator = Paginator(post_list, per_page)
+    page_number = request.GET.get('page')
+    try:
+        posts = paginator.page(page_number)
+    except PageNotAnInteger:
+        posts = paginator.page(1)
+    except EmptyPage:
+        posts = paginator.page(paginator.num_pages)
+    return render(request, 'blog/post/list.html', {'posts': posts, 'tag':tag})
+
+
+def post_detail(request, year, month, day, post):
+    post = get_object_or_404(Post, status=Post.Status.PUBLISHED, slug=post,
+                             publish__year=year, publish__month=month, publish__day=day)
+    comments = post.comments.filter(active=True)
+    form = CommentForm()
+    post_tags_ids = post.tags.values_list('id', flat=True)
+    similar_posts = Post.published.filter(tags__in=post_tags_ids).exclude(id=post.id)
+    similar_posts = similar_posts.annotate(same_tags=Count('tags')).order_by('-same_tags', '-publish')[:4]
+    return render(request, 'blog/post/detail.html',
+                  {'post': post, 'comments': comments, 'form': form})
+
+
+@require_POST
+def post_comment(request, post_id):
+    post = get_object_or_404(Post, id=post_id, status=Post.Status.PUBLISHED)
+    comment = None
+    form = CommentForm(data=request.POST)
+    if form.is_valid():
+        comment = form.save(commit=False)
+        comment.post = post
+        comment.save()
+    return render(request, 'blog/post/comment.html', {'post': post, 'form': form, 'comment': comment})
+
+
+
